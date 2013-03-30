a90e4350d0ba94975b046f26f6c3b52d96c51f3d
sethvincent <sethvincent@gmail.com>
2013-03-30 11:18:19 -0700

    first

---
+++ template.js
@@ -0,0 +1,46 @@
+'use strict';
+
+exports.description = 'Create a basic html/css/js project.';
+
+exports.notes = 'yeah, html!';
+
+exports.after = 'Now you need to install project and dev dependencies by running:' +
+  '\n\n' +
+  'npm install && bower install';
+
+exports.warnOn = '*';
+
+exports.template = function(grunt, init, done){
+
+  init.process({ type: 'html'}, [
+    init.prompt('name'),
+    init.prompt('description'),
+    init.prompt('repository'),
+    init.prompt('homepage'),
+    init.prompt('author_name'),
+    init.prompt('author_email'),
+    init.prompt('author_url'),
+    init.prompt('licenses', 'MIT'),
+    init.prompt('node_version', '>= 0.10.0'),
+  ], function(err, props){
+    props.keywords = [];
+    props.devDependencies = {
+      'grunt': '~0.4.0',
+      'grunt-contrib-qunit': '~0.2.0',
+      'grunt-contrib-jshint': '~0.1.1'
+      'grunt-contrib-connect': '~0.1.2',
+      'grunt-contrib-livereload': '~0.1.2',
+      'grunt-regarde': '~0.1.1'
+    };
+
+    var files = init.filesToCopy(props);
+
+    init.addLicenseFiles(files, props.licenses);
+
+    init.copyAndProcess(files, props);
+
+    init.writePackageJSON('package.json', props);
+
+    done();
+  });
+};
